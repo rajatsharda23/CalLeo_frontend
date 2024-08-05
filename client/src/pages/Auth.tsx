64646d3ed25103bae033c49ac19b9a842eb9a0c9
rsharda23 <72014316+rajatsharda23@users.noreply.github.com>
2024-08-05 23:52:44 +0530

    added thoda thoda frontend

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,16 +1,20 @@
-// src/pages/Auth.tsx
-
 import React from 'react';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from "jwt-decode";
+
+const google_client_id = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
+
+
+// console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID)
 
 const Auth: React.FC = () => {
   const responseGoogle = (response: any) => {
-    console.log(response);
+    console.log(jwtDecode(response.credential));
     // Handle the Google authentication response
   };
 
   return (
-    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+    <GoogleOAuthProvider clientId={google_client_id}>
       <div className="mt-16 p-8 flex justify-center items-center flex-col">
         <h1 className="text-3xl font-playfair-display mb-4">Login with Google</h1>
         <div className="flex justify-center">
